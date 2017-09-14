f44a45c242bee9019402cd9bb3409ad4169ba33b
penbell <penbell@wisenut.co.kr>
2017-09-14 23:28:04 +0900

    메인클래스 및 am클래스 내용 변경

---
+++ test/Calculator/am.py
@@ -0,0 +1,11 @@
+class AM:
+
+	def adder(self, arg1, arg2):
+		return arg1 + arg2
+
+	def minus(self, arg1, arg2):
+		return arg1 - arg2
+
+
+#arg1, arg2 = int(input('첫번째 수 ')), int(input('두번째 수'))
+
