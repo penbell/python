48024add6729ea2095eb34979163319f3c89794c
kyh1126 <kyh901126@nate.com>
2017-09-23 22:44:20 +0900

    리팩토링

--- src/test/t_SentenceManager.py
+++ src/test/t_SentenceManager.py
@@ -1,6 +1,6 @@
 '''
 @author: 신승식, 김윤희
-@date: 2017.9.19
+@date: 2017.9.23
 @version: 1.0.1
 
 @brief: sentence manager
@@ -10,13 +10,22 @@
 '''
 import time
 import SentenceManager
-
 # 클래스 호출
 p = SentenceManager.SentenceManager()
 pos = []
-
+slist = []
+path = "C:/Program Files/Java/jdk1.7.0_55/jre/bin/server/jvm.dll"
 start = time.time()
-print(p.run(pos))
+# print(p.run(pos, slist))
+try:
+   print(p.scd_parser('DOC.SCD', slist))
+except Exception as ex:
+   print(ex)
+
+try:
+   print(p.pos_tagger('청와대 수석-보좌관 10명도 24일 재산을 신고했다. 이들의 평균재산은 12억8800만원이었다.', pos, path))
+except Exception as ex:
+   print(ex)
 
 end = time.time() - start
 print(" end: [" + str(round(end,2)) + " sec]")
