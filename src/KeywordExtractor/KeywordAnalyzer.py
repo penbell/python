ac0359ef4d119835d8cd01ab5d8d43148ee64fc1
user-PC\user <wogur379@naver.com>
2017-09-28 23:21:01 +0900

    리턴 수정

--- src/KeywordExtractor/KeywordAnalyzer.py
+++ src/KeywordExtractor/KeywordAnalyzer.py
@@ -24,70 +24,78 @@ class KeywordAnalyzer() :
         try :
             self.tf_calc(list1)
             self.idf_calc()
-            self.tfidf_calc()
+            self.tfidf_calc(keyword)
             self.reset()
-            self.normalizer(self.each,keyword)
+            return 1 , 'ok'
 
-            return 1
-
-        except :
-
-            return 0
+        except Exception as ex:
+            return 0, ex
 
     def tf_calc(self, list1) :
         # tf를 계산하는 함수.
-
-        for i in range(len(list1)) :
-            self._tf += [ Counter(list1[i]) ] # 1개의 기사에서 tf값을 구하고 _tf에 dic를 리스트로 저장.
-
+        try :
+            for i in range(len(list1)) :
+                self._tf += [ Counter(list1[i]) ] # 1개의 기사에서 tf값을 구하고 _tf에 dic를 리스트로 저장.
+        except Exception as ex:
+            print(ex)
+            return 0
     def idf_calc(self) :
         '''
         실제로 df를 계산하는 함수.
         중복된 단어를 제거하고 _df변수에 df값을 채워넣는다.
         '''
-
-        isolate = Counter()
-
-        for i in range(len(self._tf)):
-            isolate += self._tf[i]
-
-        for key in isolate.keys() :
+        try:
+            isolate = Counter()
 
             for i in range(len(self._tf)):
+                isolate += self._tf[i]
 
-                if key in self._tf[i]: #isolate.keys()가 _tf[i]에 들어있으면 if 실행.
-                                        #즉, 단어가 n기사에 들어 있으면 카운팅 해주는 것.
-                    if key in self._df :
-                        self._df[key] += 1
+            for key in isolate.keys() :
 
-                    else :
-                        self._df[key] = 1
+                for i in range(len(self._tf)):
 
-    def tfidf_calc(self) :
+                    if key in self._tf[i]: #isolate.keys()가 _tf[i]에 들어있으면 if 실행.
+                                            #즉, 단어가 n기사에 들어 있으면 카운팅 해주는 것.
+                        if key in self._df :
+                            self._df[key] += 1
+
+                        else :
+                            self._df[key] = 1
+            return 1, "ok"
+        except Exception as ex:
+            return 0, ex
+    def tfidf_calc(self,keyword) :
         '''
         tfidf를 계산하는 함수.
         임시로 딕셔너리 받을 위치 생성(한 문서를 돌때마다 새로 생성.) 하고
         keyword에 값을 넣는다.
         '''
+        try:
+            for i in range(len(self._tf)) :
 
-        for i in range(len(self._tf)) :
+                each = {}
 
-            self.each = {}
+                for j in self._tf[i].keys() :
+                    each[j] = self._tf[i][j] / self._df[j]
 
-            for j in self._tf[i].keys() :
-                self.each[j] = self._tf[i][j] / self._df[j]
-        return self.each
-
+                each = self.normalizer(each)
+                keyword += [sorted(each.items(), key=operator.itemgetter(1), reverse=True)]
+            return 1, "ok"
+        except Exception as ex:
+            print(ex)
+            return 0, ex
 
     def reset(self) :
         self._tf = []
 
 # func normalizer # tf-idf 점수 평준화(0~100점)
-    def normalizer(self,dict,keyword):
-        max_value = max(dict.values())
+    def normalizer(self,k_dict):
+        max_value = max(k_dict.values())
 
-        for key in dict.keys():
-            dict[key] = int((dict[key] / max_value) * 100)
+        for key in k_dict.keys():
+             k_dict[key] = int((k_dict[key] / max_value) * 100)
+        return k_dict
 
-        keyword += [sorted(dict.items(), key=operator.itemgetter(1), reverse=True)]
+
+
 
