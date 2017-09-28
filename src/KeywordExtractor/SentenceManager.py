492542c074f374ccbd76ef3199ebedd3088e7bff
kyh1126 <kyh901126@nate.com>
2017-09-28 22:41:06 +0900

    2주차 커밋

--- src/KeywordExtractor/SentenceManager.py
+++ src/KeywordExtractor/SentenceManager.py
@@ -1,36 +1,37 @@
+# - *- coding: utf- 8 - *-
 '''
-@author : 신승식, 김윤희
-@date : 2017. 9. 19
-@version : 1.0.0
+@author : 조수현, 김윤희
+@date : 2017. 9. 26
+@version : 2.0.0
 
 @brief : 기능1
-- 파라미터 - scd:파일명, sentence:문장
-
+- 파라미터 - scd:파일명, slist:문장, pos:명사목록, path:jvm.dll경로
+             keyword:키워드,tfidf목록, id_m:키워드,채번 목록, iid_m:채번,키워드 목록
+             index:키워드가 속한 문서들 목록
 '''
 from konlpy.tag import Twitter
 from konlpy.utils import pprint
 
 class SentenceManager() :
-    
-    def run(self, pos) :                                #문장 처리 수행
+    _count = 0
+
+    def run(self, pos) :
         path = "C:/Program Files/Java/jdk1.7.0_55/jre/bin/server/jvm.dll"
         slist = []
         try :
             self.scd_parser('DOC1.SCD', slist)    # scd에서 문장 추출, #id, date는 별도 저장
-            self.pos_tagger(slist[0:10000], pos, path)    # 문장 형태소 분석하여 명사 추출
+            self.pos_tagger(slist[0:10], pos, path)    # 문장 형태소 분석하여 명사 추출
             
             return 1
         except :
             return 0
             
-
     def scd_parser(self, scd, slist) :
         sentence = open(scd,'r')
         try:
             for i in sentence :
                 if i.startswith('<Contents>') :
-                    slist.append(i[9:])
-                    # slist.append(i.replace('<Contents>',''))
+                    slist.append(i.replace('<Contents>',''))
             return 1
         except Exception as ex :
             print(ex)
@@ -38,8 +39,13 @@ class SentenceManager() :
 
     def pos_tagger(self, slist, pos, path) :
         twitter = Twitter(path)
-
         try:
+            # slist의 기사가 1개인경우 type이 str임으로 별도처리
+            if type(slist) == str:
+                pos.append(twitter.nouns(slist))
+                return 1
+
+            print("len(slist) : ",len(slist))
             for one in slist :
                 pos.append(twitter.nouns(one))
             return 1
@@ -47,7 +53,27 @@ class SentenceManager() :
             print(ex)
             return 0
 
-    # def id_manager(self) :
-    #     return
 
-    # def indexer(self) :
+    def id_manager(self, keyword, id_m, iid_m) :
+        for i in range(len(keyword)):
+            for j in range(len(keyword[i])):
+                # print(keyword[i][j][0])
+                key = keyword[i][j][0]
+                if key in id_m:
+                    continue
+                
+                id_m[key] = self._count
+                iid_m[self._count] = key
+
+                self._count += 1
+
+    def indexer(self, keyword, index) :
+        for i in range(len(keyword)):
+            for j in range(len(keyword[i])):
+                key = keyword[i][j][0]
+
+                if key in index:
+                    index[key] += [i]
+                    continue
+                
+                index[key] = [i]
--- src/test/t_SentenceManager.py
+++ src/test/t_SentenceManager.py
@@ -1,10 +1,10 @@
+# - *- coding: utf- 8 - *-
 '''
-@author: 신승식, 김윤희
-@date: 2017.9.23
-@version: 1.0.1
+@author: 조수현, 김윤희
+@date: 2017.9.28
+@version: 2.0.1
 
 @brief: sentence manager
--python version:3.5.2
 -Use class: SentenceManager
 -test parameter: arg1, arg2
 '''
@@ -15,17 +15,46 @@ p = SentenceManager.SentenceManager()
 pos = []
 slist = []
 path = "C:/Program Files/Java/jdk1.7.0_55/jre/bin/server/jvm.dll"
+
+id_m = {}
+iid_m = {}
+keyword = [[('1,10,-1,10,3.4,-10.2', 1.0), ('안녕', 0.6666666666666666), ('하세요', 0.5)], [('suhyun', 1.0), ('안녕', 1.0)], [('반갑습니다.', 1.0), ('하세요', 0.5), ('안녕', 0.3333333333333333)], [('하세요', 0.25), ('안녕', 0.16666666666666666)], [('suhyun', 0.5), ('안녕', 0.5)], [('반갑습니다.', 0.5), ('하세요', 0.25), ('안녕', 0.16666666666666666)], [('하세요', 0.14285714285714285), ('안녕', 0.1111111111111111)], [('1,10,-1,10,3.4,-10.2', 0.5), ('안녕', 0.2222222222222222), ('하세요', 0.14285714285714285)], [('반갑습니다.', 0.3333333333333333), ('하세요', 0.14285714285714285), ('안녕', 0.1111111111111111)], [('하세요', 0.1111111111111111), ('안녕', 0.08333333333333333)], [('1,10,-1,10,3.4,-10.2', 0.3333333333333333), ('안녕', 0.16666666666666666), ('하세요', 0.1111111111111111)], [('suhyun', 0.3333333333333333), ('안녕', 0.25)]]
+index = {} # 키워드 별 문서 id indexing	
+
 start = time.time()
+
 # print(p.run(pos, slist))
+
 try:
    print(p.scd_parser('DOC1.SCD', slist))
+   print(slist[0])
 except Exception as ex:
    print(ex)
 
+print("________________________________________")
+print()
+
 try:
-   print(p.pos_tagger('청와대 수석-보좌관 10명도 24일 재산을 신고했다. 이들의 평균재산은 12억8800만원이었다.', pos, path))
+   print(p.pos_tagger(slist[0:1], pos, path))
+   print(pos)
 except Exception as ex:
    print(ex)
+
+# try:
+#     p.id_manager(keyword, id_m, iid_m)
+#     print(id_m)
+#     print(iid_m)
+# except Exception as ex:
+#     print(ex)
+
+# print("________________________________________")
+# print()
+
+# try:
+#     p.indexer(keyword, index)
+#     print(index)
+# except Exception as ex:
+#     print(ex)
 
 end = time.time() - start
 print(" end: [" + str(round(end,2)) + " sec]")
